6bf6f3bf752787b3aa0736b548a8442cf0af18e3
Jackkell <bronzeeagle1@hotmail.com>
2016-11-05 23:56:42 -0700

    Separated the client and server classes from the files that build them

--- client.py
+++ client.py
@@ -3,54 +3,9 @@
 # Imports
 from csv import reader
 from sys import argv
-from keyValNode import KeyValNode
+from keyValClient import KeyValClient
 
-
-# KeyValClient
-# serverAddress: (string) the address of the server
-# port: (int) the port number for communications
-class KeyValClient(KeyValNode):
-    def __init__(self, serverAddress, port):
-        super().__init__(port)
-        self.serverAddress = serverAddress
-
-    # sends a get request
-    def get(self, key):
-        outMessage = self.encodeMessage(command="get", key=key)
-        inMessage = self.sendMessage(outMessage, self.serverAddress)
-        value = inMessage["value"]
-        success = bool(inMessage["success"])
-
-        if success:
-            print(str(key), ":", str(value), "retrieved")
-        else:
-            print(str(key), "not retrieved")
-
-    # sends a put request
-    def put(self, key, value):
-        outMessage = self.encodeMessage(command="put", key=key, value=value)
-        inMessage = self.sendMessage(outMessage, self.serverAddress)
-        success = bool(inMessage["success"])
-
-        if success:
-            print(str(key), ":", str(value), "added")
-        else:
-            print(str(key), ":", str(value), "not added")
-
-    # sends a delete request
-    def delete(self, key):
-        success = False
-
-        outMessage = self.encodeMessage(command="delete", key=key)
-        inMessage = self.sendMessage(outMessage, self.serverAddress)
-        success = bool(inMessage["success"])
-
-        if success:
-            print(str(key) + " removed")
-        else:
-            print(str(key) + " not removed")
-
-
+# A function that creates an instance of a KeyValClient
 def main():
     arguments = argv[1:]
     serverAddress = ""
