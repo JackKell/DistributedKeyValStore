3af7dd13d7988802cb9e059c9eb8a35b694a9f5f
Jackkell <bronzeeagle1@hotmail.com>
2016-11-06 18:33:29 -0700

    wrote a client that gets the event times by request

--- clientAnalysis.py
+++ clientAnalysis.py
@@ -1,9 +1,10 @@
 #!./env/bin/python3
-
 # Imports
 from csv import reader
+import csv
 from sys import argv
 from keyValClient import KeyValClient
+from time import time
 
 # A function that creates an instance of a KeyValClient
 def main():
@@ -20,18 +21,39 @@ def main():
 
     keyValClient = KeyValClient(serverAddress, port)
 
+    putcsv = open("output/put.csv", "w")
+    getcsv = open("output/put.csv", "w")
+    deletecsv = open("output/put.csv", "w")
+
+    putwriter = csv.writer(putcsv)
+    getwriter = csv.writer(getcsv)
+    deletewriter = csv.writer(deletecsv)
+
     with open(testOperationsPath, newline="\n") as operationsFile:
         operationReader = reader(operationsFile, delimiter=",")
         for operation in operationReader:
             command = operation[0]
             if command == "PUT":
+                start = time() * 1000
                 keyValClient.put(operation[1], operation[2])
+                end = time() * 1000
+                putwriter.writerow([end - start])
             elif command == "GET":
+                start = time() * 1000
                 keyValClient.get(operation[1])
+                end = time() * 1000
+                getwriter.writerow([end - start])
             elif command == "DELETE":
+                start = time() * 1000
                 keyValClient.delete(operation[1])
+                end = time() * 1000
+                deletewriter.writerow([end - start])
             else:
                 print(command + " is not a valid command")
+
+    putcsv.close()
+    getcsv.close()
+    deletecsv.close()
 
 
 if __name__ == "__main__":
